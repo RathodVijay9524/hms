9caff7ffa2e56986d91a955aae1fbecd75fa9a5e
Admin <omvijay44@gmail.com>
2025-12-24 15:56:21 +0530

    first commit

---
+++ src/main/java/com/vijay/User_Master/controller/WebController.java
@@ -0,0 +1,39 @@
+package com.vijay.User_Master.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/forgot-password")
+    public String forgotPassword() {
+        return "forgot-password";
+    }
+
+    @GetMapping("/reset-password")
+    public String resetPassword(@RequestParam("uid") Long uid, @RequestParam("token") String token, Model model) {
+        model.addAttribute("uid", uid);
+        model.addAttribute("token", token);
+        return "reset-password";
+    }
+
+    @GetMapping("/verify-account")
+    public String verifyAccount(@RequestParam("uid") Long uid, @RequestParam("code") String code, Model model) {
+        model.addAttribute("uid", uid);
+        model.addAttribute("code", code);
+        return "verify-account";
+    }
+}
